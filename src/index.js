6bdf895fe903cc2412333c06fde041de0bf18746
Anuraj123 <anurajsinghparmar1310@gmail.com>
2023-12-23 13:28:33 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-const express=require("express");
-const {PORT} =require('./config/serverconfig')
-const bodyParser=require("body-parser")
+const express = require("express");
+const { PORT } = require("./config/serverconfig");
+const bodyParser = require("body-parser");
 
 // const router = express.Router();
-const ApiRoutes=require('./routes/index')
+const ApiRoutes = require("./routes/index");
+const db=require('./models/index')
+const setupAndStartServer = async () => {
+  const app = express();
 
-const setupAndStartServer=async () =>{
-const app=express();
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use('/api',ApiRoutes);
-app.listen(PORT,()=>{
-    console.log(`Server started at ${PORT}`)
-})
-}
-setupAndStartServer(); 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api", ApiRoutes);
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+    if(process.env.SYN_DB)
+    {
+        db.sequelize.sync({alter:true})
+    }
+  });
+};
+setupAndStartServer();
